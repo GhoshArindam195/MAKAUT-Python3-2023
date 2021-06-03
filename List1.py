8007f499ebfe7d24bd24ebd654b52f2edf07708b
Arindam <ghosharindam195@gmail.com>
2021-06-03 20:32:08 +0530

    03-06-21

---
+++ List1.py
@@ -0,0 +1,20 @@
+# grocery=["Harpic", "Vim bar", "deo", "Aloo", 56, 78.9]
+# print(grocery)
+# print(grocery[0])
+# print(grocery[6])
+
+# numbers=[2, 7, 9, 1, 3, 11]
+# numbers.sort()
+# print(numbers)
+# numbers.reverse()
+# print(numbers)
+
+numbers=[2, 7, 9, 1, 3, 11]
+print(numbers[0:5])
+print(numbers[0:])
+print(numbers[ :6])
+print(numbers[:])
+print(numbers[::1])
+print(numbers[::2])
+print(numbers[::15])
+print(numbers[::-1])
---
+++ StringFunc2.py
@@ -0,0 +1,28 @@
+# str="Arindam Ghosh"
+# str1=str.lower()
+# print(str1)
+# print(str)
+# print('H'.lower())
+
+# txt='I like banana'
+# x=txt.replace('b', "c")
+# print(x)
+
+# txt="Hello, Welcome to python3"
+# print(txt.startswith("Hello"))
+# print(txt.startswith("Hello."))
+# print(txt.startswith("Hello,"))
+# print(txt.startswith("Hello, Welcome to python3"))
+
+#leading trailing
+# txt="       Hello Akash       "
+# txt=txt.strip()
+# print(txt)
+# txt=",,,rttrg,,Hello Akash,,,.....rtggff"
+# txt=txt.strip(",rtg.f")
+# print(txt)
+
+
+# txt="Hello, My Name is Jit Sarkar."
+# txt1=txt.swapcase()
+# print(txt1)
